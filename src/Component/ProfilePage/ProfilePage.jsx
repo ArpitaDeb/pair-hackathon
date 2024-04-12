dc90555685c91611773df198bf129272f5838328
padmalatha k <padmalathatce@gmail.com>
2024-04-12 13:39:50 -0700

    Updated values

--- src/Component/ProfilePage/ProfilePage.jsx
+++ src/Component/ProfilePage/ProfilePage.jsx
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ProfilePage.scss";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import padma from "../../images/Padma.jpg";
 import Arpita from "../../images/Arpita.jpg";
+import emailjs from "@emailjs/browser";
 
 function ProfilePage() {
   const personId = useParams();
   const [data, setdata] = useState(null);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_5n4jsp5", "template_m710k76", form.current, {
+        publicKey: "W5LkSrZ67dUK3zoam",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+    e.target.reset();
+  };
 
   useEffect(() => {
     try {
@@ -29,38 +49,49 @@ function ProfilePage() {
   }
 
   return (
-    <div className="card">
-      <div className="card__item card__item--change" key={data.id}>
-        <div className="card__body">
-          <div className="imgbox">
-            {console.log(data.id, typeof data.id)}
-            <img
-              src={Number(data.id) === 0 ? padma : Arpita}
-              alt=""
-              className="img"
-            ></img>
-          </div>
-          <h2 className="card__title">{data.name}</h2>
-          <p>{data.title}</p>
-          <p>{data.emal}</p>
-          <p>{`Skill Sets: ${data.skills}\n`}</p>
-          <h3>EXPERIENCE</h3>
-          <div>
-            <ul>
-              {data.experience.map((element) => {
-                return (
-                  <li>
-                    {`${element.company} , ${element.location}`}
-                    <p>{element.Duration}</p>
-                    <p>{element.description}</p>
-                  </li>
-                );
-              })}
-            </ul>
+    <>
+      <div className="card">
+        <div className="card__item card__item--change" key={data.id}>
+          <div className="card__body">
+            <div className="imgbox">
+              {console.log(data.id, typeof data.id)}
+              <img
+                src={Number(data.id) === 0 ? padma : Arpita}
+                alt=""
+                className="img"
+              ></img>
+            </div>
+            <h2 className="card__title">{data.name}</h2>
+            <p>{data.title}</p>
+            <p>{data.emal}</p>
+            <p>{`Skill Sets: ${data.skills}\n`}</p>
+            <h3>EXPERIENCE</h3>
+            <div>
+              <ul>
+                {data.experience.map((element) => {
+                  return (
+                    <li>
+                      {`${element.company} , ${element.location}`}
+                      <p>{element.Duration}</p>
+                      <p>{element.description}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="userName" required></input>
+        <label>Email</label>
+        <input type="email" name="user_email" required />
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Send" />
+      </form>
+    </>
   );
 }
 
