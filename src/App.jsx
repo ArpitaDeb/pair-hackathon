349862222b100dbb54206e2d93ac3644fda74edc
padmalatha k <padmalathatce@gmail.com>
2024-04-12 10:39:26 -0700

    color fixed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import NavBar from "./Component/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BrowserRouter> */}
+      <NavBar />
+      {/* </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <header className="navigation-wrapper">
+      <h1> Portfolio </h1>
+      <nav className="navigation__navbar">
+        <ul className="navigation__list">
+          <li className="navigation__active">
+            <a className="navigation__item">Arpita</a>
+          </li>
+          <li className="navigation__item">
+            <a className="navigation__item">Padma</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
